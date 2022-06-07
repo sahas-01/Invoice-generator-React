986123d5965fe6b64cbadfc59c85d34604e17858
Sahas <sahasvivek001@gmail.com>
2022-06-08 02:21:46 +0530

    feat: initial routes and pages added

---
+++ src/pages/InvoiceGenerator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InvoiceGenerator() {
+    return (
+        <div>Invoice Generator page</div>
+    )
+}
+
+export default InvoiceGenerator
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Landing() {
+    return (
+        <div>Landing page</div>
+    )
+}
+
+export default Landing
