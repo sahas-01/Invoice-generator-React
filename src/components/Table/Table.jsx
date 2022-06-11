a5f8845ce9fa5baf9405542bbc28d53a6aa7ffe4
Sahas <sahasvivek001@gmail.com>
2022-06-11 18:04:58 +0530

    feat: UI fixes and localstorage save

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,73 +1,76 @@
 import React, { useState } from 'react'
-import axios from 'axios'
 import { useEffect } from 'react'
 import './Table.css'
 
-function Table({ data, totalAmount, setTotalAmount }) {
+function Table({ data, totalAmount, setTotalAmount, list, setList }) {
     var totalPrice;
-    // const [total, setTotal] = useState(0);
-    // console.log(props);
     const [description, setDescription] = useState("Product 1")
     const [quantity, setQuantity] = useState(5)
+    const [selectedOption, setSelectedOption] = useState("");
     const [rate, setRate] = useState(0)
     const [amount, setAmount] = useState(1000)
-    const [selectedOption, setSelectedOption] = useState("");
-    // console.log(data)
-    const [list, setList] = useState([{
-        description,
-        quantity,
-        rate,
-        amount
-    }])
+
     useEffect(() => {
         let rows = document.querySelectorAll(".qty-amount")
         let sum = 0
 
         for (let i = 0; i < rows.length; i++) {
             if (rows[i].className === "qty-amount") {
-                sum += isNaN(rows[i].value) ? 0 : parseInt(rows[i].value)
+                sum += isNaN(rows[i].innerHTML) ? 0 : parseInt(rows[i].innerHTML)
                 setTotalAmount(sum)
             }
         }
     })
 
+    useEffect(() => {
+        const newList = [...list]
+        newList.push({
+            selectedOption,
+            quantity,
+        }
+        )
+        setList(newList)
+    }
+        , [selectedOption, quantity])
+
     // calcAmount(amount)
 
     return (
         <>
+            <>
 
-            <tbody className="table-body">
-                <tr>
-                    <td>
-                        <select className="description-dropdown" value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
-                            {
-                                data.map(item => {
-                                    return (
-                                        <option key={item.id} value={item.title}>{item.title}</option>
+                <tbody className="table-body">
+                    <tr>
+                        <td>
+                            <select className="description-dropdown font-bold" value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+                                {
+                                    data.map(item => {
+                                        return (
+                                            <option key={item.id} value={item.title} className="font-bold">{item.title}</option>
+                                        )
+                                    }
                                     )
                                 }
-                                )
+                            </select>
+                        </td>
+                        <td>{
+                            data.map(item => {
+                                if (item.title === selectedOption) {
+                                    totalPrice = parseInt(item.price) * quantity
+                                    return (
+                                        <div className="item-price font-bold">
+                                            {item.price}
+                                        </div>
+                                    )
+                                }
                             }
-                        </select>
-                    </td>
-                    <td>{
-                        data.map(item => {
-                            if (item.title === selectedOption) {
-                                totalPrice = parseInt(item.price) * quantity
-                                totalAmount = totalAmount + totalPrice
-                                return (
-                                    <>
-                                        {item.price}
-                                    </>
-                                )
-                            }
-                        }
-                        )
-                    }</td>
-                    <td><input type="number" className="qty-rate" value={quantity} onChange={(e) => { setQuantity(e.target.value) }} /></td>
-                    <td><input type="number" className="qty-amount" value={totalPrice} /></td>
-                </tr>
-            </tbody>
+                            )
+                        }</td>
+                        <td><input type="number" className="qty-rate font-bold" value={quantity} onChange={(e) => { setQuantity(e.target.value) }} /></td>
+                        <td><p className="qty-amount" style={{ fontWeight: 'bold' }} >{totalPrice}</p></td>
+                    </tr>
+                </tbody>
+            </>
         </>
     )
 }
--- src/pages/InvoiceGenerator/InvoiceGenerator.jsx
+++ src/pages/InvoiceGenerator/InvoiceGenerator.jsx
@@ -5,40 +5,62 @@ import Table from '../../components/Table/Table'
 import axios from 'axios'
 import './InvoiceGenerator.css'
 import '../../components/Table/Table.css'
+import { FaRegEdit } from "react-icons/fa";
+import { FaEye } from "react-icons/fa"
 
 function InvoiceGenerator() {
     const [data, setData] = useState([])
-    const invoices = [{
-        title: "Product 1",
-        price: 1000,
-        amount: 1000,
-        quantity: 5,
-    }];
-    console.log(invoices);
+    const [list, setList] = useState([])
     const [components, setComponents] = useState(["Sample Component"]);
     const [totalAmount, setTotalAmount] = useState(0);
-    function addComponent() {
 
+    function addComponent(e) {
+        e.preventDefault();
         setComponents([...components, "Sample Component"])
 
     }
 
-    console.log(components)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let o = Object.fromEntries(Object.entries(list).filter(([_, v]) =>
+            v.selectedOption !== "" && v.quantity !== "" && v.quantity !== 0 && isNaN(v.totalAmount)
+
+        ));
+        // console.log(o)
+        // console.log(list);
+        localStorage.setItem("list", JSON.stringify(o));
+
+    }
+
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_BACKEND_URL}/products?limit=10`)
             .then(res => {
                 setData(res.data);
-                console.log(res.data);
+                // console.log(res.data);
             })
             .catch(err => {
                 console.log(err);
             })
     }, []);
-    console.log(data);
+    // console.log(data);
+
+
     return (
         <>
             <Navbar />
-            <main className="mt-20 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow main-content">
+            <form className="mt-20 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow main-content" onSubmit={handleSubmit}>
+                {/* Edit and Preview buttons */}
+                <div className="flex justify-center">
+
+                    <button style={{ display: 'inline-block', color: '#9E9E9E' }} className="hover:text-textpurple">
+                        <FaEye style={{ display: 'inline-block', cursor: 'pointer', color: '#9E9E9E', marginRight: '10px' }} />
+                        Preview
+                    </button>
+                    <button style={{ display: 'inline-block', color: '#9E9E9E' }} className="hover:text-textpurple">
+                        <FaRegEdit style={{ display: 'inline-block', cursor: 'pointer', color: '#9E9E9E', marginRight: '7.5px', marginLeft: '10px' }} />
+                        Edit
+                    </button>
+                </div>
                 <div className="line bg-textpurple" style={{ width: '100%', height: '20px' }}>
 
                 </div>
@@ -49,7 +71,7 @@ function InvoiceGenerator() {
                 </section>
 
                 <button
-                    className='bg-textpurple hover:bg-hoverpurple text-white font-bold py-2 -mt-16 px-8 mr-20 float-right rounded'
+                    className='bg-textpurple hover:bg-hoverpurple text-white font-bold py-2 -mt-16 px-8 mr-20 float-right rounded add-item-btn'
                     onClick={addComponent}
                 >
                     Add Item
@@ -61,12 +83,18 @@ function InvoiceGenerator() {
                             <td className="font-bold text-textpurple">Description</td>
                             <td className="font-bold text-textpurple">Rate</td>
                             <td className="font-bold text-textpurple">Quantity</td>
-                            <td className="font-bold text-textpurple">Amount</td>
+                            <td className="font-bold text-textpurple">Line Total</td>
                         </tr>
                     </thead>
                     {components.map((component, index) => {
                         return (
-                            <Table data={data} totalAmount={totalAmount} setTotalAmount={setTotalAmount} />
+                            <Table
+                                data={data}
+                                totalAmount={totalAmount}
+                                setTotalAmount={setTotalAmount}
+                                list={list}
+                                setList={setList}
+                            />
                         )
 
                     }
@@ -83,8 +111,13 @@ function InvoiceGenerator() {
                     }}> Total: &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; {totalAmount}</p>
                 </section>
 
+                <section className="m-4">
+                    <button className="bg-textpurple add-item-btn hover:bg-hoverpurple text-white font-bold py-2 px-8 mr-20 float-left rounded">
+                        Save
+                    </button>
+                </section>
 
-            </main>
+            </form>
         </>
     )
 }
